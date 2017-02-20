34a692fbe2c96a29cb3f0e4bd6d74476a332a869
ClaytonTaylor <claytaylor@cox.net>
2017-02-19 18:27:27 -0600

    Almost Done

--- lib.js
+++ lib.js
@@ -1,1 +1,9 @@
 console.log("JS is Loading")
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen(){
+  this.classList.toggle('open');
+}
+
+panels.ForEach(panel => panel.addEventListener('click', toggleOpen));
